e7ebaca0d4dbaa04f0f50edcf81bede3a9bc32ee
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-08-27 08:44:46 +0200

    solution--ssr-use-hybrid-rendering

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,8 @@ import { BookEditComponent } from './books/book-edit.component';
 import { BookListComponent } from './books/book-list.component';
 
 export const routes: Routes = [
-  { path: '', component: BookListComponent },
+  { path: '', redirectTo: '/books', pathMatch: 'full' },
+  { path: 'books', component: BookListComponent },
   { path: 'books/:id', component: BookDetailComponent },
   { path: 'books/:id/edit', component: BookEditComponent },
   { path: '**', redirectTo: '' }
