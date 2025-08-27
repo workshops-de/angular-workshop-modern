601ad434484d08a9eebfb37e6510a6847e7c3fac
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-08-27 05:00:35 +0200

    solution--signals-toSignal-effect

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,14 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(),
     provideAnimations()
   ]
