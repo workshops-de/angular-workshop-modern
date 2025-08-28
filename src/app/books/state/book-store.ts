b41603df39eee6fe38db902285f9bfa85b35c128
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-08-28 08:21:49 +0200

    solution--ngrx-signal-store-use-redux-devtools

--- src/app/books/state/book-store.ts
+++ src/app/books/state/book-store.ts
@@ -1,5 +1,6 @@
 import { signalStore } from '@ngrx/signals';
 
+import { withDevtools } from '@angular-architects/ngrx-toolkit';
 import { withBooks } from './plugins/with-books';
 
-export const BookStore = signalStore({ providedIn: 'root' }, withBooks());
+export const BookStore = signalStore({ providedIn: 'root' }, withBooks(), withDevtools('books'));
