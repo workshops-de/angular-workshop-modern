fc7d2a4f858168525aa21a8f72f91a041311414b
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-08-28 19:48:57 +0200

    solution--ngrx-signal-store-immutables-state-call-state

--- src/app/books/state/plugins/with-books.ts
+++ src/app/books/state/plugins/with-books.ts
@@ -1,14 +1,7 @@
+import { setLoaded, setLoading, withCallState, withImmutableState } from '@angular-architects/ngrx-toolkit';
 import { httpResource } from '@angular/common/http';
 import { computed, inject } from '@angular/core';
-import {
-  patchState,
-  signalStoreFeature,
-  withComputed,
-  withHooks,
-  withMethods,
-  withProps,
-  withState
-} from '@ngrx/signals';
+import { patchState, signalStoreFeature, withComputed, withHooks, withMethods, withProps } from '@ngrx/signals';
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
 import { debounceTime, exhaustMap, pipe, tap } from 'rxjs';
 import { ToastService } from '../../../shared/toast.service';
@@ -16,14 +9,12 @@ import { Book } from '../../book';
 import { BookApiClient } from '../../book-api-client.service';
 
 type BookStoreState = {
-  isLoading: boolean;
   currentBookId: string | null;
   searchTerm: string;
   pageSize: number;
 };
 
 const initialState: BookStoreState = {
-  isLoading: false,
   currentBookId: null,
   searchTerm: '',
   pageSize: 10
@@ -31,7 +22,8 @@ const initialState: BookStoreState = {
 
 export function withBooks() {
   return signalStoreFeature(
-    withState(initialState),
+    withImmutableState(initialState),
+    withCallState(),
     withProps(store => ({
       booksResource: httpResource<Book[]>(
         () => `http://localhost:4730/books?_limit=${store.pageSize()}&q=${store.searchTerm()}`
@@ -43,15 +35,16 @@ export function withBooks() {
     withMethods((store, client = inject(BookApiClient), toast = inject(ToastService)) => ({
       updateBook: rxMethod<Book>(
         pipe(
-          tap(() => patchState(store, { isLoading: true })),
+          tap(() => patchState(store, setLoading())),
+          debounceTime(2_000), // used to see the loading state of the button in the UI
           exhaustMap(book => client.updateBook(book)),
           tap({
             next: () => {
-              patchState(store, { isLoading: false });
+              patchState(store, setLoaded());
               toast.show('Book updated successfully!');
             },
             error: () => {
-              patchState(store, { isLoading: false });
+              patchState(store, setLoaded());
               toast.show('Error updating book. Please try again.', 5000);
             }
           })
