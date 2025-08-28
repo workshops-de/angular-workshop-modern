0d62fa9a404f999c69e7b41dbc1dcb917306eb98
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-08-28 21:58:04 +0200

    solution--ssr-prerender-route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about.component';
 import { BookDetailComponent } from './books/book-detail.component';
 import { BookEditComponent } from './books/book-edit.component';
 import { BookListComponent } from './books/book-list.component';
@@ -8,5 +9,6 @@ export const routes: Routes = [
   { path: 'books', component: BookListComponent },
   { path: 'books/:id', component: BookDetailComponent },
   { path: 'books/:id/edit', component: BookEditComponent },
+  { path: 'about', component: AboutComponent },
   { path: '**', redirectTo: '' }
 ];
